27ec9b7c3a9f4974d4e3e209ff1e08c87a37aae7
ken_ellingsen <ken.ellingsen@gmail.com>
2024-08-27 16:01:40 -0500

    Allows user to add new food items to list

--- src/App.js
+++ src/App.js
@@ -4,13 +4,13 @@ import "./styles.css";
 
 export default function App() {
 
-  const [foodsToTry, setFoodsToTry] = useState([]);
-  const addFoodItem = (foodItem) => setFoodsToTry(...foodsToTry.push(foodItem));
+  const [foodsToTry, setFoodsToTry] = useState([]); // Initiates foodsToTry state
+  const addFoodItem = (foodItem) => setFoodsToTry([...foodsToTry, foodItem]); // Adds food item to foodsToTry array
 
   return (
     <div className="App">
       <h1>New Foods to Try</h1>
-      <NewFoodInput />
+      <NewFoodInput addFoodItem={addFoodItem} />
       <ul className="food-list">
         {foodsToTry.map((food, index) =>(
             <li key={index}>{food}</li>
--- src/NewFoodInput.js
+++ src/NewFoodInput.js
@@ -1,12 +1,18 @@
 import React, { useState } from "react";
 
-const NewFoodInput = () => {
+const NewFoodInput = (props) => {
 
   const [food, setFood] = useState(""); // Initiates food state
   const updateFood = (e) => setFood(e.target.value); // Allows state to be changed with user input
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    props.addFoodItem(food);
+    setFood("");
+  };
+
   return (
-    <form className="food-form">
+    <form className="food-form" onSubmit={handleSubmit}>
       <input type="text" value={food} onChange={updateFood} />
       <button>Add to the list!</button>
     </form>
