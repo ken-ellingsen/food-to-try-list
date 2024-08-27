0ea90e1040939630c140fbbf012380c200d18c97
ken_ellingsen <ken.ellingsen@gmail.com>
2024-08-27 15:49:17 -0500

    Created the foodsToTry state

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import NewFoodInput from "./NewFoodInput";
 import "./styles.css";
 
 export default function App() {
+
+  const [foodsToTry, setFoodsToTry] = useState([]);
+  const addFoodItem = (foodItem) => setFoodsToTry(...foodsToTry.push(foodItem));
+
   return (
     <div className="App">
       <h1>New Foods to Try</h1>
       <NewFoodInput />
-      <ul className="food-list"></ul>
+      <ul className="food-list">
+        {foodsToTry.map((food, index) =>(
+            <li key={index}>{food}</li>
+        ))}
+      </ul>
     </div>
   );
 }
