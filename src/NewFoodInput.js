8d7d7965672b0e46cca22e76e4872393950bf862
ken_ellingsen <ken.ellingsen@gmail.com>
2024-08-27 15:34:06 -0500

    Created food state

--- src/NewFoodInput.js
+++ src/NewFoodInput.js
@@ -1,9 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 
 const NewFoodInput = () => {
+
+  const [food, setFood] = useState(""); // Initiates food state
+  const updateFood = (e) => setFood(e.target.value); // Allows state to be changed with user input
+
   return (
     <form className="food-form">
-      <input type="text" />
+      <input type="text" value={food} onChange={updateFood} />
       <button>Add to the list!</button>
     </form>
   );
