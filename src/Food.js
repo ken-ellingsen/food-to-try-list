f6e33d7da3683b8a683e0346f0a701d514f69bc7
ken_ellingsen <ken.ellingsen@gmail.com>
2024-08-28 16:34:22 -0500

    Added operators and formatting to cross tried items off of list without removing

--- src/Food.js
+++ src/Food.js
@@ -1,14 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Food = (props) => {
+
+    const [tried, setTried] = useState(false);
 
     const onDelete = () => {
         props.deleteFood(props.food);
     };
 
     return (
-    <li>
-        {props.food}
+    <li className={tried && 'gray'} onClick={() => setTried(!tried)}>
+        <span className={tried && 'strike'}>{props.food}</span>
         <span className="delete" onClick={onDelete}>
             X
         </span>
